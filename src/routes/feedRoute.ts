8a5ff3e8054a2c1e3d506c280d22b2433dae414f
agrippa <afsalahamed07@gmail.com>
2025-01-11 15:26:21 +0530

    chunked http response.

--- src/routes/feedRoute.ts
+++ src/routes/feedRoute.ts
@@ -10,23 +10,41 @@ feedRoute.get("/", async (c) => {
   // and well inthe front end
   try {
     const rssList: (typeof rss.$inferSelect)[] = await db.select().from(rss);
-    const data = await Promise.all(
-      rssList.map(async (rssItem) => {
-        try {
-          const response = await fetch(rssItem.link); // Assuming `rssItem` contains a `url` field
-          const text = await response.text();
-          return { link: rssItem.link, data: text };
-        } catch (fetchError: any) {
-          // NOTE: here we should try to cach specific errors rather than
-          // generics
-          return { link: rssItem.link, error: fetchError.message };
-        }
-      }),
-    );
 
-    return c.json({ data: data }, 200, {
-      "Content-Type": "application/json",
+    const encoder = new TextEncoder();
+    c.header("Content-Type", "application/json");
+    c.header("Transfer-Encoding", "chunked");
+
+    const stream = new ReadableStream({
+      async start(controller) {
+        controller.enqueue(encoder.encode("["));
+
+        for (let i = 0; i < rssList.length; i++) {
+          const rssItem = rssList[i];
+          try {
+            const response = await fetch(rssItem.link); // Assuming `rssItem` contains a `url` field
+            const text = await response.text();
+            const chunk = JSON.stringify({ link: rssItem.link, data: text });
+
+            controller.enqueue(encoder.encode(chunk));
+            if (i < rssList.length - i) {
+              controller.enqueue(encoder.encode(","));
+            }
+          } catch (fetchError: any) {
+            const errorChunk = JSON.stringify({
+              link: rssItem.link,
+              error: fetchError.message,
+            });
+            controller.enqueue(encoder.encode(errorChunk));
+          }
+        }
+
+        controller.enqueue(encoder.encode("]"));
+        controller.close();
+      },
     });
+
+    return c.body(stream, 200);
   } catch (error) {
     return c.json({ msg: "Error occured fetchgin saveed feeds list" }, 500);
   }
