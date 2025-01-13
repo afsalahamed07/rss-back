83d66779979bf23f16f01cb08f1197a0828339b4
agrippa <afsalahamed07@gmail.com>
2025-01-13 18:39:43 +0530

    entry schema

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -5,3 +5,20 @@ export const rss = sqliteTable("rss", {
   title: text("title"),
   link: text("link").unique().notNull(),
 });
+
+export const entry = sqliteTable("entry", {
+  id: integer("id").primaryKey(),
+  title: text("title"),
+  link: text("link"),
+  pubDate: text("pubDate"),
+  creator: text("creator"),
+  content: text("content"),
+  contentSnippet: text("contentSnippet"),
+  guid: text("guid"),
+  categories: text("categories"),
+  isoDate: text("isoDate"),
+  subject: text("subject"),
+  author: text("author"),
+  feedUrl: text("feedUrl"),
+  domain: text("domain"),
+});
